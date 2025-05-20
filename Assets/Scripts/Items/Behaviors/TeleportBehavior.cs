4df2026dc64ef0057cff9cf0e9f0b00f3d19973b
Nikolajs <nikolajskiselovs@gmail.com>
2025-05-20 19:07:32 +0300

    Commit v4

--- Assets/Scripts/Items/Behaviors/TeleportBehavior.cs
+++ Assets/Scripts/Items/Behaviors/TeleportBehavior.cs
@@ -3,12 +3,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TeleportBehavior", menuName = "Items/Behaviors/Teleport")]
 public class TeleportBehavior : ItemBehaviorBase
 {
-    [SerializeField] private Transform _teleportPosition; //Improve
+    [SerializeField] private Vector3 _center = Vector3.zero;
+    [SerializeField] private Vector3 _range = new Vector3(50, 0, 50);
 
     public override void Use(ItemUseContext itemUseContext)
     {
         Transform playerTransform = itemUseContext.Player.transform;
 
-        playerTransform.position = _teleportPosition.position;
+        Vector3 randomOffset = new Vector3(
+            Random.Range(-_range.x, _range.x),
+            Random.Range(-_range.y, _range.y),
+            Random.Range(-_range.z, _range.z)
+        );
+
+        Vector3 targetPosition = _center + randomOffset;
+        playerTransform.position = targetPosition;
     }
 }
